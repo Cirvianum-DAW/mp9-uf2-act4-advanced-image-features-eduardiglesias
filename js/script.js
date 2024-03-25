85bcac840d948d4aadc074652c8b91ee519ad6ab
eduard <eduard.iglesias@cirvianum.cat>
2024-03-25 17:53:46 +0100

    lightbox, exercise 6 finished

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
-// This is the JavaScript file for the project
+document.addEventListener('DOMContentLoaded', function () {
 
-// Write your JavaScript code here
-// You can manipulate the DOM, handle events, and work with images
+  const images = [
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg',
+    'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg',
+  ];
 
-// Example code to load an image dynamically
-const image = document.createElement('img');
-image.src = 'path/to/image.jpg';
-document.body.appendChild(image);
+  const galleryContainer = document.querySelector('.gallery-container');
 
-// Example code to handle a button click event
-const button = document.querySelector('#myButton');
-button.addEventListener('click', () => {
-  // Do something when the button is clicked
+  images.forEach((imageUrl) => {
+    const anchor = document.createElement('a');
+    anchor.setAttribute('data-lightbox', 'gallery');
+    anchor.setAttribute('data-title', 'Image');
+    anchor.href = imageUrl;
+
+    const image = document.createElement('img');
+    image.src = imageUrl;
+    image.classList.add('gallery-image');
+
+    anchor.appendChild(image);
+    galleryContainer.appendChild(anchor);
+  });
+
+  // Exemple de codi per gestionar un esdeveniment de clic de botó
+  const button = document.querySelector('#myButton');
+  button.addEventListener('click', () => {
+    // Fer alguna cosa quan es faci clic al botó
+  });
 });
